b64b3a29817f2cd3c8e06ad60c0668936e948086
Nildae80 <nildae80@gmail.com>
2026-09-17 21:52:02 -0500

    Fix typo in file uploader function name

--- app.py
+++ app.py
@@ -3,5 +3,4 @@ import streamlit as st
 st.title("Manejo de Dataframes")
 st.sidebar.title("Herramientas")
 
-archivo = st.sidebar.file_uploder("Selecciona tu archivo a cargar")
-
+archivo = st.sidebar.file_uploader("Selecciona tu archivo a cargar")
