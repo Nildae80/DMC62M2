088b8b538ee88534010512a35f6e6d7e9adade37
Nildae80 <nildae80@gmail.com>
2026-09-17 21:56:43 -0500

    Fix file extension checks for uploaded files

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ st.sidebar.title("Herramientas")
 archivo = st.sidebar.file_uploader("Selecciona tu archivo a cargar")
 if archivo in not None:
   sr.write("Su archivo fue cargado exitosamente")
-  if archivo.name.endswith(.csv):
+  if archivo.name.endswith(".csv"):
     datos = pd.read_csv(archivo)
-  if archivo.name.endswith(.xls):
+  if archivo.name.endswith(".xls"):
     datos = pd.read_excel(archivo)
   st.write(datos)
 else:
