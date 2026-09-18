15d729893fe8fede5b0211d205a4ebb9af781a9a
Nildae80 <nildae80@gmail.com>
2026-09-17 21:55:43 -0500

    Enhance file upload functionality in app.py
    Fix file upload check and add support for CSV and XLS files.

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Manejo de Dataframes")
 st.sidebar.title("Herramientas")
 
 archivo = st.sidebar.file_uploader("Selecciona tu archivo a cargar")
+if archivo in not None:
+  sr.write("Su archivo fue cargado exitosamente")
+  if archivo.name.endswith(.csv):
+    datos = pd.read_csv(archivo)
+  if archivo.name.endswith(.xls):
+    datos = pd.read_excel(archivo)
+  st.write(datos)
+else:
+  sr.write("Cargue su archivo")
